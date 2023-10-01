1d676e9a6a4f41088e31cc979e80699e4a44a9e6
DarkNaegleria <fvillalazm@outlook.com>
2023-10-01 10:07:12 -0500

    Add new views edit profile and change password

---
+++ resources/views/dashboard/profile/change-password.blade.php
@@ -0,0 +1,71 @@
+@extends('adminlte::page')
+
+@section('title', 'Mi Cuenta')
+
+@section('content_header')
+<!-- ESPACIO ARRIBA -->
+@stop
+
+@section('content')
+    @if (session('status') === 'password-updated')
+        @section('js')
+            <script>
+                Swal.fire(
+                    'Listo!',
+                    'Tu contraseña ha sido actualizada!',
+                    'success'
+                )
+            </script>   
+        @stop  
+    @elseif (session('status') === 'password-error')
+        @section('js')
+            <script>
+                Swal.fire({
+                    type: 'error',
+                    title: 'Oops...',
+                    text: 'Tu contraseña actual es incorrecta o intentas usar la misma como la nueva!',
+                })
+            </script>   
+        @stop  
+    @endif
+
+    <div class="container my-4 px-4 mx-1 flex justify-center">
+        <div class="card" style="width: 40rem;">
+            <div class="card-body">
+                <div class="card-header flex justify-content-center border-bottom border-danger bg-red-50">
+                    <h1 class="card-title text-center" style="font-weight: 600">Edición de Seguridad</h1>
+                </div>
+                <form id="cambiar-contrasena-form" action="{{ route('profile.updatePassword') }}" method="post">
+                    @csrf
+                    <div class="form-group mt-2.5">
+                        <label for="currentPassword">Contraseña Actual</label>
+                        <input type="password" class="form-control" id="currentPassword" name="currentPassword" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="newPassword">Nueva Contraseña</label>
+                        <input type="password" class="form-control" id="newPassword" name="newPassword" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmNewPassword">Confirmar Nueva Contraseña</label>
+                        <input type="password" class="form-control" id="confirmNewPassword" name="confirmNewPassword" required>
+                    </div>
+                    <div class="mt-4 flex justify-content-end">
+                        <button type="submit" class="btn btn-outline-success">Guardar y regresar</button>
+                        <a href="{{route('profile.index')}}" role="button" class="btn btn-outline-secondary ml-md-2">Cancelar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+@stop
+
+@section('css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@stop
+
+@section('js')
+  
+    
+@stop
+
---
+++ resources/views/dashboard/profile/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('adminlte::page')
+
+@section('title', 'Mi Cuenta')
+
+@section('content_header')
+<!-- ESPACIO ARRIBA -->
+@stop
+
+@section('content')
+
+    <div class="container my-4 px-4 mx-1 flex justify-center">
+        <div class="card" style="width: 40rem;">
+            <div class="card-body">
+                <div class="card-header flex justify-content-center border-bottom border-success bg-green-50">
+                    <h1 class="card-title text-center" style="font-weight: 600">Edición de la Cuenta</h1>
+                </div>
+                <form id="editar-nombre-form" method="post" action="{{ route('profile.update') }}">
+                    @csrf
+                    <div class="form-group mt-2.5">
+                        <label for="firstName">Nombre(s)</label>
+                        <input type="text" class="form-control" id="firstName" name="firstName" value="{{ old('firstName', $user->firstName) }}" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastName">Apellido(s)</label>
+                        <input type="text" class="form-control" id="lastName" name="lastName" value="{{ old('lastName', $user->lastName) }}" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Correo Electrónico</label>
+                        <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $user->email) }}" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">Número de Celular</label>
+                        <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone', $user->phone) }}" required>
+                    </div>
+                    <div class="mt-4 flex justify-content-end">
+                        <button type="submit" class="btn btn-outline-success">Guardar y regresar</button>
+                        <a href="{{route('profile.index')}}" role="button" class="btn btn-outline-secondary ml-md-2">Cancelar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@stop
+
+@section('css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@stop
+
+@section('js')
+  
+    
+@stop
+

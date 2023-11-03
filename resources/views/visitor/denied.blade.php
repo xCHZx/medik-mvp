3121e74bbe46185b999f13cc25b9f92ed179337f
DarkNaegleria <fvillalazm@outlook.com>
2023-11-02 21:42:21 -0500

    Vistas visitor, basic css

--- resources/views/visitor/denied.blade.php
+++ resources/views/visitor/denied.blade.php
@@ -1,9 +1,9 @@
 <x-visitor-layout>
-<div class="mb-8">
+<div class="mb-8 mx-3 text-center">
     <h2>¡No puedes registrarte a {{$business["name"]}}! ⛔</h2>
 </div>
-<div class="mb-8">
-    <h5>No han pasado 12 horas desde tu visita anterior.</h5>
+<div class="mb-8 mx-3 text-center">
+    <h5>Aún no han pasado 12 horas desde tu visita anterior.</h5>
 </div>
 <div>
     <div class="card">
--- resources/views/visitor/visit.blade.php
+++ resources/views/visitor/visit.blade.php
@@ -1,29 +1,29 @@
 <x-visitor-layout>
-<div class="mb-8">
+<div class="mb-8 mx-3 text-center">
     <h2>¡Te damos la bienvenida a {{$business["name"]}}! 👋</h2>
 </div>
-<div>
+<div class="mx-3">
     <div class="card">
         <div class="card-body">
             <form class="p-3"  method="POST" action="{{route('visitor.store', $id)}}">
             @csrf
-                <div class="row mb-3">
+                <div class="row">
                     <div class="form-group col-md-6">
                         <label for="firstName" class="form-label">Nombre</label>
                         <input type="text" class="form-control"  name="firstName" id="firstName" required>
                     </div>
                     <div class="form-group col-md-6">
-                        <label for="lastName" class="form-label">Apellido</label>
+                        <label for="lastName" class="form-label mt-3">Apellido</label>
                         <input type="text" class="form-control"  name="lastName" id="firstName" required>
                     </div>
                 </div>
                     <div class="form-group col-md-12">
-                        <label for="phone" class="form-label">Teléfono</label>
+                        <label for="phone" class="form-label mt-3">Teléfono</label>
                         <input type="number" class="form-control"  name="phone" id="phone" required>
                     </div>
-                <div class="mt-3">
+                <div class="mt-4 text-center col-12">
                     <input name="businessId" value="{{ $id }}" type="hidden" />
-                    <button type="submit" class="btn btn-success">Enviar</button>
+                    <button type="submit" class="btn btn-success col-12">Enviar</button>
                 </div>
               </form>
         </div>

038d58943b81ffc1d1edbab5c9c1a484c2481d38
xCHZx <chzx94@gmail.com>
2023-11-14 19:26:44 -0500

    Disable migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,15 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(UserSeeder::class);
-        $this->call(BusinessSeeder::class);
-        $this->call(VisitSeeder::class);
+        // $this->call(UserSeeder::class);
         // $this->call(BusinessSeeder::class);
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // $this->call(VisitSeeder::class);
     }
 }

03b5160f368353eef604c09827545e1325ef25a2
DarkNaegleria <fvillalazm@outlook.com>
2023-11-14 19:12:46 -0500

    Forgot password view created

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,56 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+<x-visitor-layout>
 
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    @if (session('status'))
+        <div class="mb-4 text-green-600">
+            {{ session('status') }}
+        </div>
+    @endif
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <div class="text-center mb-1">
+        <!-- Manito saludona -->
+        <span class="text-4xl">&#128543;</span>
+
+        <!-- Título -->
+        <div class="flex justify-content-center align-content-center mt-2">
+            <h1 class="text-2xl font-bold fs-4 text-gray-600 mt-3" style="padding-top: 0.13rem">
+                ¿Olvidaste tu contraseña de
+                <!-- Logo Medik -->
+                <x-logo-full fill-1="#18CCAB" fill-2="#0F0E4B" class="ml-1 mb-4 py-0 d-inline" width="100" height="50"/>
+                ? 
+            </h1>
+            
+        </div>
+
+        <!-- Descripción -->
+        <p class="text-gray-600 mt-2">¡No hay problema! <br>
+            Déjanos tu correo electrónico y te ayudaremos a restablecerla.
+        </p>
+    </div>
+
+
+    <form class="w-full sm:max-w-lg mt-2 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg" method="POST" action="{{ route('password.email') }}">
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <div class="mb-4">
+            <label for="email" class="block font-medium text-sm text-gray-600 ">Correo Electrónico</label>
+            <input id="email" placeholder="email@example.com" class="form-control focus:border-indigo-500 focus:ring-indigo-500 shadow-sm block mt-1 w-full" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex flex-col justify-content-between align-items-center mt-4 d-block d-sm-flex">
+            <div class="col-12">
+                <button class="px-4 py-2 mdkbtn-success rounded-md focus:outline-none focus:ring focus:ring-indigo-500 w-full">
+                    Quiero recuperar mi contraseña!
+                </button>
+            </div>
+
+            <div class="col-12 mt-2 text-center">
+                <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-indigo-500 focus:ring-indigo-500" href="{{ route('login') }}">
+                    Ya recordé mi contraseña! &#128073;&#128072; 
+                </a>
+            </div>
         </div>
     </form>
-</x-guest-layout>
+</x-visitor-layout>

564d9f5c9f672750ff71e59e79af262cc94a9af1
DarkNaegleria <fvillalazm@outlook.com>
2023-11-22 22:45:01 -0500

    Add reports cards WIP

--- resources/views/dashboard/reports/index.blade.php
+++ resources/views/dashboard/reports/index.blade.php
@@ -7,41 +7,83 @@
 @stop
 
 @section('content')
-    <p><b>Negocio:</b></p>
-    <p>{{$business->name}}</p>
-    <hr>
-    <p><b>Puntuación:</b></p>
-    <p>{{number_format($business->averageRating, 1)}}</p>
-    <hr>
-    <form action="{{ route('reports.index') }}" method="GET">
-        <input type="date" name="startDate" placeholder="Fecha de inicio">
-        <input type="date" name="endDate" placeholder="Fecha de finalización">
-        <button class="btn btn-outline-primary" type="submit">Filtrar</button>
-    </form>
-    <a href="{{ route('reports.index') }}" class="btn btn-outline-danger">Limpiar</a>
-    <hr>
+    <section id="resumen" class="grid grid-cols-4 gap-5">
+        <div class="card basis-1/4">
+            <div class="card-body">
+                <h6 class="font-medium">Negocio Activo</h6>
+                <p class="my-4 font-medium card-text">{{$business->name}}</p>
+                <label class="rating-label d-flex flex-column justify-content-center align-items-center text-4xl">
+                    {{number_format($business->averageRating, 1)}}
+                    <input
+                    name="rating"
+                    class="rating mt-2"
+                    max="5"
+                    oninput="this.style.setProperty('--value', `${this.valueAsNumber}`)"
+                    style="--value:{{number_format($business->averageRating, 1)}}; --starsize: 2rem"
+                    type="range"
+                    disabled
+                >
+                </label>
+            </div>
+        </div>
+        <div class="card basis-1/4">
+            <div class="card-body">
+                <h6 class="font-medium text-lg">Total Opiniones</h6>
+                <div class="row my-2">
+                    <div>                
+                        <p class="card-text text-6xl">{{count($allReviews)}}</p>
+                        <div class="p-1 rounded-md" style="background: #FFEFE7;">
+                            <p class="text-xs">+{{$lastMonthReviewsVariation}}% Mes pasado</p>
+                        </div>
+                    </div>
+                    <div class="ml-0 pl-1">
+                        <p class="text-sm text-gray-500">{{count($goodReviews)}} Positivas</p>
+                        <p class="text-sm text-gray-500">{{count($badReviews)}} Negativas</p>
+                    </div>
+                </div>
+                <a href="#" class="btn mdkbtn-primary mt-2 p-2 w-full text-center">Ver más</a>
+            </div>
+        </div>
+        <div class="card basis-1/4">
+            <div class="card-body">
+                <h6 class="font-medium text-lg">Total Clientes</h6>
+                <div class="row my-2">
+                    <div>                
+                        <p class="card-text text-6xl">{{count($allReviews)}}</p> <!--Aquí cambiar-->
+                        <div class="p-1 rounded-md" style="background: #FFEFE7;">
+                            <p class="text-xs">+{{$lastMonthReviewsVariation}}% Mes pasado</p> <!--Aquí cambiar-->
+                        </div>
+                    </div>
+                    <div class="ml-0 pl-1">
+                        <p class="text-sm text-gray-500">{{count($goodReviews)}} Recurrentes</p> <!--Aquí cambiar-->
+                    </div>
+                </div>
+                <a href="#" class="btn mdkbtn-primary mt-2 p-2 w-full text-center">Ver más</a>
+            </div>
+        </div>
+        <div class="card basis-1/4">
+            <div class="card-body">
+                <h6 class="font-medium text-lg">Total Visitas</h6>
+                <div class="col my-2">
+                    <p class="card-text text-6xl">{{count($allVisits)}}</p>
+                    <div class="p-1 rounded-md w-32" style="background: #FFEFE7;">
+                        <p class="text-xs">+{{$lastMonthVisitsVariation}}% Mes pasado</p>
+                    </div>
+                </div>
+                <a href="#" class="btn mdkbtn-primary p-2 mt-2 w-full text-center">Ver más</a>
+            </div>
+        </div>
+    </section>
 
+    <section id="search" class="d-flex justify-start">
+        <form action="{{ route('reports.index') }}" method="GET">
+            <input type="date" name="startDate" placeholder="Fecha de inicio">
+            <input type="date" name="endDate" placeholder="Fecha de finalización">
+            <button class="mdkbtn-success py-2 w-24 ml-md-2" type="submit">Filtrar</button>
+        </form>
+        <a href="{{ route('reports.index') }}" class="mdkbtn-danger py-2 w-24 text-center ml-md-2">Limpiar</a>
+    </section>
 
-    <p><b>Total Opiniones:</b></p>
-    <p>{{count($allReviews)}}</p>
-    <hr>
-    <p>+{{$lastMonthReviewsVariation}}% en los últimos 30 días</p>
-    <hr>
-    <p><b>Total Opiniones Positivas:</b></p>
-    <p>{{count($goodReviews)}}</p>
-    <hr>
-    <p><b>Total Opiniones Negativas:</b></p>
-    <p>{{count($badReviews)}}</p>
-    <hr>
-    <p><b>Total de Visitas:</b></p>
-    <p>{{count($allVisits)}}</p>
-    <hr>
-    <p>+{{$lastMonthVisitsVariation}}% en los últimos 30 días</p>
-    <hr>
-    <br>
-    <hr>
-    <br>
-    <hr>
     @if (request()->has('startDate') && request()->has('endDate'))
         <p><b>Fecha de inicio:</b> {{request()->startDate}}</p>
         <br>
